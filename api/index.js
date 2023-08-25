31fa3412b04fa5b3b28dbaad2b5da2ad4a915c33
SaurabhMor2000 <115579361+SaurabhMor2000@users.noreply.github.com>
2023-08-25 14:07:10 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,15 +14,15 @@ const uploadMiddleware = multer({dest : 'uploads/'});
 const secret =  'qwertyuiopasdfghjkl';
 const fs = require('fs');
 
-const corsOptions = {
-  credentials: true,
-   // Allow requests from this origin
-};
+// const corsOptions = {
+//   credentials: true,
+//    // Allow requests from this origin
+// };
 app.use((req, res, next) => {
   res.header({"Access-Control-Allow-Origin": "*"});
   next();
 }) 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
